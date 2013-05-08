9dc7ee1eccb462db0e6d96428fcdc8eaba14d44b
Pavel Simakov <psimakov@google.com>
2013-05-07 23:19:38 -0700

    Added the custom tag for embedding forum in lesson pages 	Change on 2013/04/24 by akadakia <akadakia@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=45735890

--- coursebuilder/extensions/tags/gcb/__init__.py
+++ coursebuilder/extensions/tags/gcb/__init__.py
@@ -14,8 +14,9 @@
 
 """GCB-provided custom tags."""
 
-__author__ = 'John Orr (jorr@google.com)'
+__author__ = 'John Orr (jorr@google.com)', 'Aparna Kadakia (akadakia@google.com)'
 
+import urllib
 from common import schema_fields
 from common import tags
 from lxml import etree
@@ -95,3 +96,81 @@ ykXhHPKj8AAAAASUVORK5CYII=
             optional=True,
             description='Provide YouTube video ID (e.g. Kdg2drcUjYI)'))
         return reg
+
+
+class ForumEmbed(tags.BaseTag):
+    def render(self, node):
+        forum_name = node.attrib.get('forum')
+        category_name = node.attrib.get('category')
+        embedded_forum_url = (
+            'https://groups.google.com/forum/embed/?place=forum/?'
+            'fromgroups&hl=en#!categories/%s/%s') \
+            % (urllib.urlencode(forum_name),urllib.urlencode(category_name))
+        iframe = etree.XML("""
+<p>
+  <iframe class="forum-embed" title="Forum Embed"
+    type="text/html" width="700" height="300" frameborder="0">
+  </iframe>
+</p>""")
+        iframe[0].set('src', embedded_forum_url)
+        return iframe
+
+    def get_icon_url(self):
+        """Return the URL for the icon to be displayed in the rich text editor.
+
+        In this example, the icon is encoded into the URL using the 'data'
+        production, but the URL could equally well point to the location of
+        an image file hosted on the server."""
+
+        return """
+data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBg4NDQ0NDQ8ODQ8MDQ0ND
+xQODQ4NEA4NFRQhFBQQFBIYGyYeGBkkHBUSHy8gIzMpLCwtFR4xNTEsNSYrLCoBCQoKDgwOGg8PGikfH
+yA1LDYuNS80KSwpKTU1Lik1LC0sNTAwLCw1LSo0My8vLjYyKSkpLDUqMSwsKTE2LC4pLP/AABEIAHgAo
+AMBIgACEQEDEQH/xAAbAAEAAgMBAQAAAAAAAAAAAAAAAgYBBAcFA//EAD8QAAIBAgEHBgkLBQAAAAAAA
+AABAgMRBAUGEiExUWEHE0Fxc7EUNGKRk7LB0dIWFyIyM0JSVIGh8COCg5LC/8QAGwEBAAICAwAAAAAAA
+AAAAAAAAAQGAgMBBQf/xAAxEQACAQICBQoHAQAAAAAAAAAAAQIDBAUREjRxkdEUFSExUVKBobHBEyIyQ
+UNT4SX/2gAMAwEAAhEDEQA/AO4gAAAAAAAAAAAAGLgGQa1TKNCDalVpRa31IJ+a5GOVcO9lei/8sPeZa
+Euww049qNsEYzTV07p7taMpmJmZAAAAAAAAAAAAAPnWrRhGU5tRjFXbepJcThvLpY6z6A8SWd+EX35vq
+pSsR+WWE/FU9HIi8tt/2R3ok8kr9x7j3SM5qKbbSSV23qSR4nyzwn4p+jkeBl/OXwh6FNyVFb005ve1u
+4Gynd205ZOrFeKNdW3uIRzVOTexnrZUzvUbww6UvLl9X+1dPWVvF5SrVvtKk5X6L2j/AKrUabroi66O6
+pX2G010VYb0dFWs8Rqv5qc9zyJ3Isg66IuuiUsWsf3R3oiPCr39M9zNijip0nenOVN+RJx7j3cmZ6VYN
+RxC52OzSikprjuZW9K+ww2TZUadaObSeZBhXq0JfK2svt/DqmDx1OvBVKUlOL3dD3NdDNg5fknLFTCVV
+ODbi7Kcb6px9j3M6Tg8XCtThVpu8ZxUl7nxOhurV0H2plis7xXCyfRJH3ABDJ4AAAAAAPAz0k1hF5VWC
+fFa2e+V7PbxWPbQ7mQsQ1aewl2WsQ2lHbIthsi2UBFyDZhsw2RbMkgGzDYbItmYDZFsNkWzJI5Nig9X6
+kmz50HqfWTbPYcD1Cls9zx7HOjEKu32Rhst+YePb57Dt3StVhwu7SX7xf6spzZ72ZDfhqts5qpfq1e2x
+NvYKVCWZBsajjcRy+/QdEABVi4gAAAAAAr2e/ise2h3MsJXc+PFI9tDuZCxDVp7CXZaxDaURsi2GyLZQ
+0i5Bsw2YbItmQMtkWw2RbMkjkNkWw2RbM0gbFB6n1k2z5UHqfWTbPX8D1Cls92eOY8/9Crt9kGy35gYF
+3rYl7GlRjx+9J+qvOVnJeTp4qtGjCyctbb2Ritr49R1LAYKFClClTVo01Zb3vb4szxK4UYfDXW/Qxwq2
+c6nxX1L1/hsAAr5ZwAAAAAAVzPrxSPbQ7mWM1MpZOhiaUqVS+jK2zbFrZJcSPdUnVoyguto329RU6sZv
+qTOUtkWy3y5PZX1YhW40Xf1jHzeT/MR9C/iKjzXddzzXEtHONt3vJ8CntmGy3/N3P8AMx9C/iMPk6n0Y
+iHoZL/oy5suu55riOcbbveT4FPbItn0xWHnSnOnUWjOEnGSfQz4tkLRyeTJyaazQbItnr5vZAePnUgqi
+pc3GMruLne7tbaj3Y8msr68TG3Ci/jJlKyrVY6UI5rwItW8o0paM5ZPxKrgqM6l4whKbvshGUn5kZnFx
+bUk01qaas0+KOq5JyRTwlGNGlfRjdtv60pPbJksdkihiFatThPoTatJdUlrR6Hh15yahCjOP0o85xOw5
+VcTrQl9T+5yzB42dCrCrTdpU5XW570+DOn5HyxSxdJVKb1qynFv6UJbn7+k8TF8n1CWulVqUuEkqsfY/
+wBzVw+ZOLw9RVMPioRktV9Ccbrc1rTXAm3NS3uY5qWUl25+ZBtKV1aSycdKL7Gt6LqDXwcKqhHnnTlNb
+XTUoxfUnsNg6VrIsCeazAAODkAAAAAAAAAAAAqmeub3PU/CaSvVpL6aW2pTXtXcc8bO2s5rnpm94NU5+
+krUazd0tlOrtceCe1ebcV/FLP8ANBbeJYMLvPwz8OHA3OTf7bE9lT9Zl/Of8mz/AK2J7Kn6zOgE/DNWj
+4+pAxPWZeHoAAdidcAAAAAAAAAAAAAAAAAAAAADXx+DhXpTo1VpQqLRa9q4mwYZw0msmcptPNFNzPyTP
+B47GUZ67UqcoS2adPSdpfzpLmQ5tX0rK9rX6bbbEzTQoqjDQXUbq9Z1p6cuvoAAN5oAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAP/2Q==
+"""
+
+    def get_schema(self):
+        """Return the list of fields which will be displayed in the editor.
+
+        This method assembles the list of fields which will be displayed in
+        the rich text editor when a user double-clicks on the icon for the tag.
+        The fields are a list of SchemaField objects in a FieldRegistry
+        container. Each SchemaField has the actual attribute name as used in the
+        tag, the display name for the form, and the type (usually string)."""
+        reg = schema_fields.FieldRegistry('Forum')
+        reg.add_property(
+            schema_fields.SchemaField(
+              'forum', 'Forum Name', 'string', optional=True,
+              description='Name of the Forum (e.g. mapping-with-google)'))
+        reg.add_property(
+            schema_fields.SchemaField(
+              'category', 'Category Name', 'string', optional=True,
+              description='Name of the Category (e.g. unit5-2-annotation)'))
+        return reg
+
