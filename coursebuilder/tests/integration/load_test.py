6d3a05aebbc166a649b656e7c9464c6f333a59e3
Pavel Simakov <psimakov@google.com>
2013-05-07 23:25:22 -0700

    Added response time histogram. 	Change on 2013/04/26 by psimakov <psimakov@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=45845974

--- coursebuilder/tests/integration/load_test.py
+++ coursebuilder/tests/integration/load_test.py
@@ -76,6 +76,7 @@ class WebSession(object):
     GET_COUNT = 0
     POST_COUNT = 0
     PROGRESS_BATCH = 10
+    RESPONSE_TIME_HISTOGRAM = [0, 0, 0, 0, 0, 0]
     PROGRESS_LOCK = threading.Lock()
 
     def __init__(self, common_headers=None):
@@ -85,13 +86,33 @@ class WebSession(object):
         self.cj = cookielib.CookieJar()
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
 
-    def log_progress(self):
-        update = ((WebSession.GET_COUNT + WebSession.POST_COUNT) % (
-            self.PROGRESS_BATCH) == 0)
-        if update:
+    @classmethod
+    def increment_duration_bucket(cls, index):
+        cls.RESPONSE_TIME_HISTOGRAM[index] += 1
+
+    @classmethod
+    def update_duration(cls, duration):
+        if duration > 30:
+            cls.increment_duration_bucket(0)
+        elif duration > 15:
+            cls.increment_duration_bucket(1)
+        elif duration > 7:
+            cls.increment_duration_bucket(2)
+        elif duration > 3:
+            cls.increment_duration_bucket(3)
+        elif duration > 1:
+            cls.increment_duration_bucket(4)
+        else:
+            cls.increment_duration_bucket(5)
+
+    @classmethod
+    def log_progress(cls, force=False):
+        update = ((cls.GET_COUNT + cls.POST_COUNT) % (
+            cls.PROGRESS_BATCH) == 0)
+        if update or force:
             logging.info(
-                'Progress GET/POST: %s/%s',
-                WebSession.GET_COUNT, WebSession.POST_COUNT)
+                'GET/POST:[%s/, %s], SLA:%s',
+                cls.GET_COUNT, cls.POST_COUNT, cls.RESPONSE_TIME_HISTOGRAM)
 
     def get_cookie_value(self, name):
         for cookie in self.cj:
@@ -108,7 +129,17 @@ class WebSession(object):
         request = urllib2.Request(url)
         for key, value in self.common_headers.items():
             request.add_header(key, value)
-        response = self.opener.open(request)
+
+        start_time = time.time()
+        try:
+            response = self.opener.open(request)
+        except Exception as e:
+            logging.info('Error executing GET for: %s', (url))
+            raise e
+        finally:
+            with WebSession.PROGRESS_LOCK:
+                self.update_duration(time.time() - start_time)
+
         assert_equals(expected_code, response.code)
         return response.read()
 
@@ -124,7 +155,17 @@ class WebSession(object):
         request = urllib2.Request(url, data)
         for key, value in self.common_headers.items():
             request.add_header(key, value)
-        response = self.opener.open(request)
+
+        start_time = time.time()
+        try:
+            response = self.opener.open(request)
+        except Exception as e:
+            logging.info('Error executing POST for: %s', (url))
+            raise e
+        finally:
+            with WebSession.PROGRESS_LOCK:
+                self.update_duration(time.time() - start_time)
+
         assert_equals(expected_code, response.code)
         return response.read()
 
@@ -249,9 +290,11 @@ class PeerReviewLoadTest(object):
         assessment_xsrf_token = self.get_js_var('assessmentXsrfToken', body)
 
         answers = [
-            {'index': 0, 'type': 'regex', 'value': 'blah1'},
-            {'index': 1, 'type': 'choices', 'value': None},
-            {'index': 2, 'type': 'regex', 'value': 'blah3'},
+            {'index': 0, 'type': 'regex',
+             'value': 'Answer 0 by %s' % self.email},
+            {'index': 1, 'type': 'choices', 'value': self.uid},
+            {'index': 2, 'type': 'regex',
+             'value': 'Answer 2 by %s' % self.email},
         ]
 
         data = {
@@ -304,7 +347,8 @@ class PeerReviewLoadTest(object):
 
             answers = [
                 {'index': 0, 'type': 'choices', 'value': 0},
-                {'index': 1, 'type': 'regex', 'value': 'blah2'},
+                {'index': 1, 'type': 'regex',
+                 'value': 'Review 0 by %s' % self.email},
             ]
 
             data = {
@@ -333,18 +377,31 @@ class PeerReviewLoadTest(object):
 
 def run_all(args):
     """Runs test scenario in multiple threads."""
-    if args.thread_count < 1 or args.thread_count > 100:
-        raise Exception('Please use between 1 and 100 threads.')
+    if args.thread_count < 1 or args.thread_count > 256:
+        raise Exception('Please use between 1 and 256 threads.')
 
-    logging.info('Started')
+    start_time = time.time()
+    logging.info('Started testing: %s', args.base_url)
+    logging.info('base_url: %s', args.base_url)
+    logging.info('start_uid: %s', args.start_uid)
+    logging.info('thread_count: %s', args.thread_count)
+    logging.info('SLAs are [>30s, >15s, >7s, >3s, >1s, <1s]')
     tasks = []
     WebSession.PROGRESS_BATCH = args.thread_count
     for index in range(0, args.thread_count):
         test = PeerReviewLoadTest(args.base_url, args.start_uid + index)
         task = TaskThread(test.run, name='PeerReviewLoadTest-%s' % index)
         tasks.append(task)
-    TaskThread.execute_task_list(tasks)
-    logging.info('Completed')
+
+    try:
+        TaskThread.execute_task_list(tasks)
+        logging.info('Completed!')
+    except Exception as e:
+        logging.info('Failed!')
+        raise e
+    finally:
+        WebSession.log_progress(force=True)
+        logging.info('Duration (s): %s', time.time() - start_time)
 
 
 if __name__ == '__main__':
