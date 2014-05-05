d0885fbbc6614bd0cfd7a3b4b46c03c88d882e04
Pavel Simakov <psimakov@google.com>
2014-05-05 12:41:05 -0700

    Refactor transforms.py to remove direct dependency on entities.py This is being done to permit an upcoming CL that adds a requirement in entities.py for functions in transforms.py (or entity_transforms.py) 	Change on 2014/04/09 by mgainer <mgainer@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=64659600

---
+++ coursebuilder/models/entity_transforms.py
@@ -0,0 +1,130 @@
+# Copyright 2014 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Set of converters between db models, Python and JSON dictionaries, etc."""
+
+__author__ = 'Mike Gainer (mgainer@google.com)'
+
+import base64
+import datetime
+import entities
+import transforms_constants
+
+from common import schema_fields
+
+from google.appengine.api import datastore_types
+from google.appengine.ext import db
+
+PYTHON_TYPE_TO_JSON_TYPE = {
+    basestring: 'string',
+    datetime.date: 'date',
+    datetime.datetime: 'datetime',
+    int: 'integer',
+    float: 'number',
+    bool: 'boolean',
+    datastore_types.Text: 'text',
+}
+SUPPORTED_TYPES = (
+    datastore_types.Key,
+    datetime.date,
+    datetime.datetime,
+    db.GeoPt,
+)
+
+
+def _get_schema_field(property_type):
+    name = property_type.name
+    if property_type.data_type == list:
+        # Shallow evaluation here is OK; Python DB API does not permit
+        # array-of-array; when declaring a ListProperty, the item type
+        # must be a Type instance (and thus cannot be a class, and thus
+        # cannot be a Property class)
+        item_type = schema_fields.SchemaField(
+            name=name + ':item', label=name + ':item', optional=True,
+            property_type=PYTHON_TYPE_TO_JSON_TYPE[
+                property_type.item_type])
+        ret = schema_fields.FieldArray(
+            name=name, label=name,
+            description=property_type.verbose_name,
+            item_type=item_type)
+    else:
+        ret = schema_fields.SchemaField(
+            name=name, label=name,
+            property_type=PYTHON_TYPE_TO_JSON_TYPE[property_type.data_type],
+            description=property_type.verbose_name,
+            optional=not property_type.required)
+    return ret
+
+
+def get_schema_for_entity(clazz):
+    assert issubclass(clazz, entities.BaseEntity)  # Must have blacklist.
+    available_properties = clazz.properties()
+    # Treating as module-protected. pylint: disable-msg=protected-access
+    suppressed = clazz._get_export_blacklist()
+    registry = schema_fields.FieldRegistry(clazz.__name__)
+    for property_type in available_properties.values():
+        if property_type not in suppressed:
+            registry.add_property(_get_schema_field(property_type))
+    return registry
+
+
+def entity_to_dict(entity, force_utf_8_encoding=False):
+    """Puts model object attributes into a Python dictionary."""
+    output = {}
+    for_export = isinstance(entity, entities.ExportEntity)
+    properties = entity.properties()
+
+    if for_export:
+        for name in entity.instance_properties():
+            properties[name] = getattr(entity, name)
+
+    for key, prop in properties.iteritems():
+        value = getattr(entity, key)
+        if (value is None or
+            isinstance(value, transforms_constants.SIMPLE_TYPES) or
+            isinstance(value, SUPPORTED_TYPES)):
+            output[key] = value
+
+            # some values are raw bytes; force utf-8 or base64 encoding
+            if force_utf_8_encoding and isinstance(value, basestring):
+                try:
+                    output[key] = value.encode('utf-8')
+                except UnicodeDecodeError:
+                    output[key] = {
+                        'type': 'binary',
+                        'encoding': 'base64',
+                        'content': base64.urlsafe_b64encode(value)}
+
+        else:
+            raise ValueError('Failed to encode: %s' % prop)
+
+    # explicitly add entity key as a 'string' attribute
+    output['key'] = str(entity.safe_key) if for_export else str(entity.key())
+
+    if for_export:
+        output.pop('safe_key')
+
+    return output
+
+
+def dict_to_entity(entity, source_dict):
+    """Sets model object attributes from a Python dictionary."""
+    for key, value in source_dict.items():
+        if (value is None
+            or isinstance(value, transforms_constants.SIMPLE_TYPES)
+            or isinstance(value, SUPPORTED_TYPES)):
+            setattr(entity, key, value)
+        else:
+            raise ValueError('Failed to encode: %s' % value)
+    return entity
--- coursebuilder/models/transforms.py
+++ coursebuilder/models/transforms.py
@@ -16,19 +16,21 @@
 
 __author__ = 'Pavel Simakov (psimakov@google.com)'
 
-import base64
 import datetime
 import itertools
 import json
 from xml.etree import ElementTree
-
-import entities
+import transforms_constants
 import yaml
-
-from common import schema_fields
 
 from google.appengine.api import datastore_types
 from google.appengine.ext import db
+
+# Leave tombstones pointing to moved functions
+# pylint: disable-msg=unused-import,g-bad-import-order
+from entity_transforms import dict_to_entity
+from entity_transforms import entity_to_dict
+from entity_transforms import get_schema_for_entity
 
 ISO_8601_DATE_FORMAT = '%Y-%m-%d'
 ISO_8601_DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
@@ -52,33 +54,17 @@ _JSON_DATETIME_FORMATS = [
 ]
 JSON_TYPES = ['string', 'date', 'datetime', 'text', 'html', 'boolean',
               'integer', 'number', 'array', 'object']
-PYTHON_TYPE_TO_JSON_TYPE = {
-    basestring: 'string',
-    datetime.date: 'date',
-    datetime.datetime: 'datetime',
-    int: 'integer',
-    float: 'number',
-    bool: 'boolean',
-    datastore_types.Text: 'text',
-}
-
 # Prefix to add to all JSON responses to guard against XSSI. Must be kept in
 # sync with modules/oeditor/oeditor.html.
 JSON_XSSI_PREFIX = ")]}'\n"
-SIMPLE_TYPES = (int, long, float, bool, dict, basestring, list)
-SUPPORTED_TYPES = (
-    datastore_types.Key,
-    datetime.date,
-    datetime.datetime,
-    db.GeoPt,
-)
 
 
 def dict_to_json(source_dict, unused_schema):
     """Converts Python dictionary into JSON dictionary using schema."""
     output = {}
     for key, value in source_dict.items():
-        if value is None or isinstance(value, SIMPLE_TYPES):
+        if value is None or isinstance(value,
+                                       transforms_constants.SIMPLE_TYPES):
             output[key] = value
         elif isinstance(value, datastore_types.Key):
             output[key] = str(value)
@@ -224,90 +210,6 @@ def json_to_dict(source_dict, schema):
         else:
             output[key] = source_dict[key]
     return output
-
-
-def _get_schema_field(property_type):
-    name = property_type.name
-    if property_type.data_type == list:
-        # Shallow evaluation here is OK; Python DB API does not permit
-        # array-of-array; when declaring a ListProperty, the item type
-        # must be a Type instance (and thus cannot be a class, and thus
-        # cannot be a Property class)
-        item_type = schema_fields.SchemaField(
-            name=name + ':item', label=name + ':item', optional=True,
-            property_type=PYTHON_TYPE_TO_JSON_TYPE[property_type.item_type])
-        ret = schema_fields.FieldArray(
-            name=name, label=name,
-            description=property_type.verbose_name,
-            item_type=item_type)
-    else:
-        ret = schema_fields.SchemaField(
-            name=name, label=name,
-            property_type=PYTHON_TYPE_TO_JSON_TYPE[property_type.data_type],
-            description=property_type.verbose_name,
-            optional=not property_type.required)
-    return ret
-
-
-def get_schema_for_entity(clazz):
-    assert issubclass(clazz, entities.BaseEntity)  # Must have blacklist.
-    available_properties = clazz.properties()
-    # Treating as module-protected. pylint: disable-msg=protected-access
-    suppressed = clazz._get_export_blacklist()
-    registry = schema_fields.FieldRegistry(clazz.__name__)
-    for property_type in available_properties.values():
-        if property_type not in suppressed:
-            registry.add_property(_get_schema_field(property_type))
-    return registry
-
-
-def entity_to_dict(entity, force_utf_8_encoding=False):
-    """Puts model object attributes into a Python dictionary."""
-    output = {}
-    for_export = isinstance(entity, entities.ExportEntity)
-    properties = entity.properties()
-
-    if for_export:
-        for name in entity.instance_properties():
-            properties[name] = getattr(entity, name)
-
-    for key, prop in properties.iteritems():
-        value = getattr(entity, key)
-        if value is None or isinstance(value, SIMPLE_TYPES) or isinstance(
-                value, SUPPORTED_TYPES):
-            output[key] = value
-
-            # some values are raw bytes; force utf-8 or base64 encoding
-            if force_utf_8_encoding and isinstance(value, basestring):
-                try:
-                    output[key] = value.encode('utf-8')
-                except UnicodeDecodeError:
-                    output[key] = {
-                        'type': 'binary',
-                        'encoding': 'base64',
-                        'content': base64.urlsafe_b64encode(value)}
-
-        else:
-            raise ValueError('Failed to encode: %s' % prop)
-
-    # explicitly add entity key as a 'string' attribute
-    output['key'] = str(entity.safe_key) if for_export else str(entity.key())
-
-    if for_export:
-        output.pop('safe_key')
-
-    return output
-
-
-def dict_to_entity(entity, source_dict):
-    """Sets model object attributes from a Python dictionary."""
-    for key, value in source_dict.items():
-        if value is None or isinstance(value, SIMPLE_TYPES) or isinstance(
-                value, SUPPORTED_TYPES):
-            setattr(entity, key, value)
-        else:
-            raise ValueError('Failed to encode: %s' % value)
-    return entity
 
 
 def string_to_value(string, value_type):
