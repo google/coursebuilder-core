16084ad4949a426b5b3516073dd498b2c2da5532
Pavel Simakov <psimakov@google.com>
2014-05-05 12:40:14 -0700

    Add utility class to collect exceptions/messages and retain for later reporting. 	Change on 2014/04/03 by mgainer <mgainer@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=64312195

---
+++ coursebuilder/common/catch_and_log.py
@@ -0,0 +1,125 @@
+# Copyright 2014 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module providing simplistic logger."""
+
+__author__ = 'Mike Gainer (mgainer@google.com)'
+
+import datetime
+import traceback
+
+import appengine_config
+
+_LOG_DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+_CRITICAL = 'critical'
+_WARNING = 'warning'
+_INFO = 'info'
+
+
+class CatchAndLog(object):
+    """Simplistic logger allowing WebApp handlers to note errors for consumers.
+
+    During processing of a request, there may be recoverable errors and other
+    noteworthy events.  This logger allows components to simply note these so
+    that they can be reported, rather than having to report only the first
+    problem, or trying to encode multiple events into a single HTTP response
+    code.
+    """
+
+    class _Catcher(object):
+        """Automatically note thrown exceptions as log messages."""
+
+        def __init__(self, log, consume_exceptions, message):
+            self._log = log
+            self._consume_exceptions = consume_exceptions
+            self._message = message
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, ex_type, value, tb):
+            if ex_type:
+                frame_tuple = list(traceback.extract_tb(tb)[-1])
+                frame_tuple[0] = frame_tuple[0].replace(
+                    appengine_config.CODE_ROOT, '')
+                exception_message = (
+                    '%s: %s: %s' %
+                    (self._message, ex_type.__name__, str(value)))
+                if not appengine_config.PRODUCTION_MODE:
+                    exception_message += (
+                        ' at %s' % traceback.format_list([frame_tuple])[0])
+                self._log.critical(exception_message)
+                return self._consume_exceptions
+
+    def __init__(self):
+        self._messages = []
+
+    def consume_exceptions(self, message):
+        """Convert exceptions into 'critical' log messages.
+
+        This is a convenience function for use in contexts where exceptions
+        may be raised, but are not fatal and should not propagate.  Usage:
+
+        with log.log_and_consume_exceptions("Arming mouse trap"):
+            mouse_trap.set_bait('Wensleydale')
+            mouse_trap.set_closing_force('critical personal injury')
+            mouse_trap.arm()
+
+        Args:
+          message: Prepended to exception messages to give more context.
+              E.g., suppose some calling code receives an exception:
+              OutOfCheeseException('Can't open pantry!').  That may be true,
+              neither is it very helpful.  If this is expressed as:
+              Arming mouse trap: OutOfCheeseException: Can't open pantry!
+              then the external caller has a somewhat better idea of why
+              being out of cheese is a problem.
+        Returns:
+          A context manager for use in a 'with' statement.
+        """
+        return CatchAndLog._Catcher(
+            self, consume_exceptions=True, message=message)
+
+    def propagate_exceptions(self, message):
+        """Log exceptions as 'critical' log messages, and propagate them.
+
+        See log_and_consume_exceptions() for usage.
+
+        Args:
+          message: Prepended to exception messages to give more context.
+        Returns:
+          A context manager for use in a 'with' statement.
+        """
+        return CatchAndLog._Catcher(
+            self, consume_exceptions=False, message=message)
+
+    def _log(self, level, message):
+        self._messages.append({
+            'message': message,
+            'level': level,
+            'timestamp': datetime.datetime.now().strftime(_LOG_DATE_FORMAT)})
+
+    def critical(self, message):
+        self._log(_CRITICAL, message)
+
+    def warning(self, message):
+        self._log(_WARNING, message)
+
+    def warn(self, message):
+        self._log(_WARNING, message)
+
+    def info(self, message):
+        self._log(_INFO, message)
+
+    def get(self):
+        return self._messages
---
+++ coursebuilder/tests/unit/common_catch_and_log.py
@@ -0,0 +1,140 @@
+# Copyright 2014 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Unit tests for logger."""
+
+__author__ = 'Mike Gainer (mgainer@google.com)'
+
+import datetime
+import unittest
+
+import appengine_config
+from common import catch_and_log
+
+
+class CatchAndLogTests(unittest.TestCase):
+
+    def setUp(self):
+        appengine_config.PRODUCTION_MODE = False
+        self._catch_and_log = catch_and_log.CatchAndLog()
+        self._expected = []
+
+    def test_simple(self):
+        complaint = 'No cheese, Gromit!'
+        self._catch_and_log.critical(complaint)
+        self._expect(complaint, catch_and_log._CRITICAL)
+        self._assert_logs_match()
+
+    def test_multiple(self):
+        complaints = [
+            'Failed to find cheese in pantry',
+            'Failed to install cheese to mousetrap',
+            'Failed to arm trap',
+            'Failed to catch mouse'
+            ]
+        for complaint in complaints:
+            self._catch_and_log.critical(complaint)
+            self._expect(complaint, catch_and_log._CRITICAL)
+        self._assert_logs_match()
+
+    def test_multiple_levels(self):
+        complaint = 'No cheese, Gromit!'
+        self._catch_and_log.critical(complaint)
+        self._expect(complaint, catch_and_log._CRITICAL)
+
+        complaint = 'Moon out of range!'
+        self._catch_and_log.warn(complaint)
+        self._expect(complaint, catch_and_log._WARNING)
+
+        complaint = 'Parking brake engaged!'
+        self._catch_and_log.warning(complaint)
+        self._expect(complaint, catch_and_log._WARNING)
+
+        complaint = 'Five mice spectating'
+        self._catch_and_log.info(complaint)
+        self._expect(complaint, catch_and_log._INFO)
+
+        complaint = 'Red light blinking'
+        self._catch_and_log.info(complaint)
+        self._expect(complaint, catch_and_log._INFO)
+
+        complaint = 'Low blinker fluid'
+        self._catch_and_log.warning(complaint)
+        self._expect(complaint, catch_and_log._WARNING)
+
+        complaint = 'Insufficient fuel for landing!'
+        self._catch_and_log.critical(complaint)
+        self._expect(complaint, catch_and_log._CRITICAL)
+        self._assert_logs_match()
+
+    def test_exception_suppressed(self):
+        topic = 'Entering Orbit'
+        complaint = 'Perigee below surface of Moon!'
+        with self._catch_and_log.consume_exceptions(topic):
+            raise ValueError(complaint)
+
+        self._expect(
+            '%s: ValueError: %s at   ' % (topic, complaint) +
+            'File "/tests/unit/common_catch_and_log.py", line 86, in '
+            'test_exception_suppressed\n    raise ValueError(complaint)\n',
+            catch_and_log._CRITICAL)
+        self._assert_logs_match()
+
+    def test_exception_propagates(self):
+        topic = 'Entering Orbit'
+        complaint = 'Perigee below surface of Moon!'
+        with self.assertRaises(ValueError):
+            with self._catch_and_log.propagate_exceptions(topic):
+                raise ValueError(complaint)
+
+        self._expect(
+            '%s: ValueError: %s at   ' % (topic, complaint) +
+            'File "/tests/unit/common_catch_and_log.py", line 100, in '
+            'test_exception_propagates\n    raise ValueError(complaint)\n',
+            catch_and_log._CRITICAL)
+        self._assert_logs_match()
+
+    def test_traceback_info_suppressed_in_production(self):
+        appengine_config.PRODUCTION_MODE = True
+        topic = 'Entering Orbit'
+        complaint = 'Perigee below surface of Moon!'
+        with self._catch_and_log.consume_exceptions(topic):
+            raise ValueError(complaint)
+
+        self._expect('%s: ValueError: %s' % (topic, complaint),
+                     catch_and_log._CRITICAL)
+        self._assert_logs_match()
+
+    def _expect(self, message, level):
+        self._expected.append({
+            'message': message,
+            'level': level,
+            'timestamp': datetime.datetime.now().strftime(
+                catch_and_log._LOG_DATE_FORMAT)})
+
+    def _assert_logs_match(self):
+        if len(self._expected) != len(self._catch_and_log.get()):
+            self.fail('Expected %d entries, but have %d' %
+                      (len(self._expected), len(self._catch_and_log.get())))
+
+        for expected, actual in zip(self._expected, self._catch_and_log.get()):
+            self.assertEquals(expected['level'], actual['level'])
+            self.assertEquals(expected['message'], actual['message'])
+            expected_time = datetime.datetime.strptime(
+                expected['timestamp'], catch_and_log._LOG_DATE_FORMAT)
+            actual_time = datetime.datetime.strptime(
+                actual['timestamp'], catch_and_log._LOG_DATE_FORMAT)
+            self.assertAlmostEqual(
+                0, abs((expected_time - actual_time).total_seconds()), 2)
