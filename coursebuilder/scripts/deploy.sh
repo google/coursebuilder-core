bb053475b89cfc755b97fb7b199c3f19efba7aec
Pavel Simakov <psimakov@google.com>
2014-05-10 00:15:44 -0700

    Add a deploy script to the public scripts folder. 	Change on 2014/05/09 by jorr <jorr@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=66658251

---
+++ coursebuilder/scripts/deploy.sh
@@ -0,0 +1,34 @@
+# Copyright 2014 Google Inc. All Rights Reserved.
+#
+# Usage:
+#
+# Run this script from the Course Builder folder. It can be run with the
+# following arguments:
+#
+# Deploy Course Builder to the App Engine app named in app.yaml:
+#     sh ./scripts/deploy.sh
+#
+# Deploy Course Builder to the given App Engine app:
+#     sh ./scripts/deploy.sh my_app_name
+#
+# Deploy Course Builder with optional arguments passed to appcfg.py:
+#     sh ./scripts/deploy.sh <optional_args>
+# E.g.,
+#   Authenticate on App Engine with OAuth2:
+#     sh ./scripts/deploy.sh --oauth2
+# See the documentation for appcfg.py for more details:
+#   https://developers.google.com/appengine/docs/python/tools/uploadinganapp
+
+set -e
+
+. "$(dirname "$0")/common.sh"
+
+if [[ $# == 1 && $1 != -* ]]; then
+  application="--application=$1"
+  shift
+fi
+
+python "$GOOGLE_APP_ENGINE_HOME/appcfg.py" \
+  $application \
+  "$@" update \
+  "$SOURCE_DIR"
