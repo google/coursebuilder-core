6150dfc244b0ae9106f7aeb9ed1573cabbfb68d6
johncox <johncox@google.com>
2014-09-16 09:44:06 -0700

    Update App Engine SDK from 1.9.8 to 1.9.9. 	Change on 2014/09/16 by johncox <johncox@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=75634209

--- coursebuilder/scripts/common.sh
+++ coursebuilder/scripts/common.sh
@@ -60,10 +60,10 @@ echo Using GAE from $GOOGLE_APP_ENGINE_HOME
 if [ ! -d "$GOOGLE_APP_ENGINE_HOME" ]; then
   echo Installing GAE
   mkdir -p $RUNTIME_HOME
-  wget https://storage.googleapis.com/appengine-sdks/deprecated/198/google_appengine_1.9.8.zip -O google_appengine_1.9.8.zip
-  unzip google_appengine_1.9.8.zip -d $RUNTIME_HOME/
+  wget https://storage.googleapis.com/appengine-sdks/deprecated/199/google_appengine_1.9.9.zip -O google_appengine_1.9.9.zip
+  unzip google_appengine_1.9.9.zip -d $RUNTIME_HOME/
   mv $RUNTIME_HOME/google_appengine $GOOGLE_APP_ENGINE_HOME
-  rm google_appengine_1.9.8.zip
+  rm google_appengine_1.9.9.zip
 fi
 
 function need_install() {
