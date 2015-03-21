572546446b3a4057c4102cbdf00df8379f8900be
jorr <jorr@google.com>
2015-03-20 17:22:11 -0700

    Adapt the lesson header skill display to cope with multiple lessons per page. 	Change on 2015/03/20 by jorr <jorr@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=89171413

--- coursebuilder/modules/skill_map/resources/js/lesson_header.js
+++ coursebuilder/modules/skill_map/resources/js/lesson_header.js
@@ -3,142 +3,166 @@ var ARROW_UP_CLASS = 'md-keyboard-arrow-up';
 var HIGHLIGHTED_CARD_CLASS = 'highlighted';
 var CLICKABLE_SKILL_CLASS = 'clickable';
 var SHADED_CARD_CLASS = 'shaded';
-
-var dependencyMap = null;
+var IS_INITIALIZED = 'initialized';
 
 function now() {
   return new Date().getTime();
 }
-function emitPanelOpenEvent(isOpened) {
-  var data = {
-    type: 'open',
-    isOpened: isOpened
-  };
-  gcbAudit(gcbCanPostEvents, data, 'skill-panel', true)
+
+function SkillPanel(panelDiv) {
+  this._panelDiv = panelDiv;
+  this._dependencyMap = this._panelDiv.data('dependencyMap');
 }
-function emitSkillHoverEvent(skillId) {
-  var data = {
-    type: 'skill-hover',
-    skillId: skillId
-  };
-  gcbAudit(gcbCanPostEvents, data, 'skill-panel', true)
-}
-function bindOpenButton() {
-  var detailsPanel = $('div.skill-panel div.skill-details');
-  var openButton = $('div.skill-panel div.open-control button');
-  openButton.click(function() {
-    detailsPanel.toggle(400, function() {
-      if (detailsPanel.is(':visible')) {
-        openButton.removeClass(ARROW_DOWN_CLASS);
-        openButton.addClass(ARROW_UP_CLASS);
-        $('div.skill-panel ol.skill-display-root > li.skill')
-            .addClass(CLICKABLE_SKILL_CLASS);
-        emitPanelOpenEvent(true);
-      } else {
-        openButton.removeClass(ARROW_UP_CLASS);
-        openButton.addClass(ARROW_DOWN_CLASS);
-        $('div.skill-panel ol.skill-display-root > li.skill')
-            .removeClass(CLICKABLE_SKILL_CLASS);
-        emitPanelOpenEvent(false);
+SkillPanel.prototype = {
+  emitPanelOpenEvent: function(isOpened) {
+    var data = {
+      type: 'open',
+      isOpened: isOpened
+    };
+    gcbAudit(gcbCanPostEvents, data, 'skill-panel', true)
+  },
+
+  emitSkillHoverEvent: function(skillId) {
+    var data = {
+      type: 'skill-hover',
+      skillId: skillId
+    };
+    gcbAudit(gcbCanPostEvents, data, 'skill-panel', true)
+  },
+
+  bindOpenButton: function() {
+    var that = this;
+    var detailsPanel = this._panelDiv.find('div.skill-details');
+    var openButton = this._panelDiv.find('div.open-control button');
+    openButton.click(function() {
+      detailsPanel.toggle(400, function() {
+        if (detailsPanel.is(':visible')) {
+          openButton.removeClass(ARROW_DOWN_CLASS);
+          openButton.addClass(ARROW_UP_CLASS);
+          that._panelDiv.find('ol.skill-display-root > li.skill')
+              .addClass(CLICKABLE_SKILL_CLASS);
+          that.emitPanelOpenEvent(true);
+        } else {
+          openButton.removeClass(ARROW_UP_CLASS);
+          openButton.addClass(ARROW_DOWN_CLASS);
+          that._panelDiv.find('ol.skill-display-root > li.skill')
+              .removeClass(CLICKABLE_SKILL_CLASS);
+          that.emitPanelOpenEvent(false);
+        }
+      });
+    });
+  },
+
+  highlightCards: function(skillId) {
+    var that = this;
+    var dependencyData = this._dependencyMap[skillId];
+
+    this._panelDiv.find('.skill-card').addClass(SHADED_CARD_CLASS);
+
+    $.each(dependencyData.depends_on, function() {
+      var skillId = this;
+      that._panelDiv.find('ol.depends-on .skill-card')
+          .filter(function() {
+            return $(this).data('skillId') == skillId;
+          })
+          .removeClass(SHADED_CARD_CLASS)
+          .addClass(HIGHLIGHTED_CARD_CLASS);
+    });
+
+    $.each(dependencyData.leads_to, function() {
+      var skillId = this;
+      that._panelDiv.find('ol.leads-to .skill-card')
+          .filter(function() {
+            return $(this).data('skillId') == skillId;
+          })
+          .removeClass(SHADED_CARD_CLASS)
+          .addClass(HIGHLIGHTED_CARD_CLASS);
+    });
+
+    this.emitSkillHoverEvent(skillId);
+  },
+
+  resetCardHighlights: function() {
+    this._panelDiv.find('ol.skill-display-root .skill').removeClass(HIGHLIGHTED_CARD_CLASS);
+    this._panelDiv.find('ol.skill-display-root .skill').removeClass(SHADED_CARD_CLASS);
+    this._panelDiv.find('.skill-card').removeClass(HIGHLIGHTED_CARD_CLASS);
+    this._panelDiv.find('.skill-card').removeClass(SHADED_CARD_CLASS);
+  },
+
+  bindSkillClick: function() {
+    var that = this;
+    this._panelDiv.find('div.skills-in-this-lesson .skill').click(function() {
+      if (! $(this).hasClass(CLICKABLE_SKILL_CLASS)) {
+        return;
+      }
+      that._panelDiv.find('ol.skill-display-root .skill').addClass(SHADED_CARD_CLASS);
+      $(this).removeClass(SHADED_CARD_CLASS).addClass(HIGHLIGHTED_CARD_CLASS);
+      that.highlightCards($(this).data('skillId'));
+    });
+    $(window).click(function(evt) {
+      if ($(evt.target).closest('.skill-panel ol.skill-display-root').length == 0) {
+        that.resetCardHighlights();
       }
     });
-  });
-}
-function highlightCards(skillId) {
-  var dependencyData = dependencyMap[skillId];
+  },
 
-  $('div.skill-panel .skill-card').addClass(SHADED_CARD_CLASS);
+  bindTooltips: function() {
+    this._panelDiv.find('.skills-in-this-lesson').tooltip({
+      items: 'li.skill',
+      content: function() {
+        var description = $(this).data('skillDescription').trim();
+        if (description) {
+          return ('<b>' + $(this).text().trim() + '</b>: ' + description);
+        } else {
+          return null;
+        }
+      },
+      position: {
+        my: 'center top',
+        at: 'center bottom+10'
+      },
+      tooltipClass: 'skill-panel-tooltip'
+    });
+    this._panelDiv.find('.skill-details .skill-card .description').tooltip({
+      items: '.skill-card',
+      content: function() {
+        var content = $(this).find('.content').text().trim();
+        if (content) {
+          return (
+              '<b>' + $(this).find('.name').text().trim() + '</b>: ' + content);
+        } else {
+          return null;
+        }
+      },
+      position: {
+        my: 'center top',
+        at: 'center bottom-40'
+      },
+      tooltipClass: 'skill-panel-tooltip'
+    });
+  },
 
-  $.each(dependencyData.depends_on, function() {
-    var skillId = this;
-    $('div.skill-panel ol.depends-on .skill-card')
-        .filter(function() {
-          return $(this).data('skillId') == skillId;
-        })
-        .removeClass(SHADED_CARD_CLASS)
-        .addClass(HIGHLIGHTED_CARD_CLASS);
-  });
+  init: function () {
+    var detailsPanel = this._panelDiv.find('div.skill-details');
+    var openButton = this._panelDiv.find('div.open-control button');
 
-  $.each(dependencyData.leads_to, function() {
-    var skillId = this;
-    $('div.skill-panel ol.leads-to .skill-card')
-        .filter(function() {
-          return $(this).data('skillId') == skillId;
-        })
-        .removeClass(SHADED_CARD_CLASS)
-        .addClass(HIGHLIGHTED_CARD_CLASS);
-  });
+    this.bindOpenButton();
+    this.bindSkillClick();
+    this.bindTooltips();
+    detailsPanel.hide();
+    openButton.addClass(ARROW_DOWN_CLASS);
 
-  emitSkillHoverEvent(skillId);
-}
-function resetCardHighlights() {
-  $('ol.skill-display-root .skill').removeClass(HIGHLIGHTED_CARD_CLASS);
-  $('ol.skill-display-root .skill').removeClass(SHADED_CARD_CLASS);
-  $('div.skill-panel .skill-card').removeClass(HIGHLIGHTED_CARD_CLASS);
-  $('div.skill-panel .skill-card').removeClass(SHADED_CARD_CLASS);
-}
-function bindSkillClick() {
-  $('div.skill-panel div.skills-in-this-lesson .skill').click(function() {
-    if (! $(this).hasClass(CLICKABLE_SKILL_CLASS)) {
-      return;
-    }
-    $('ol.skill-display-root .skill').addClass(SHADED_CARD_CLASS);
-    $(this).removeClass(SHADED_CARD_CLASS).addClass(HIGHLIGHTED_CARD_CLASS);
-    highlightCards($(this).data('skillId'));
-  });
-  $(window).click(function(evt) {
-    if ($(evt.target).closest('.skill-panel ol.skill-display-root').length == 0) {
-      resetCardHighlights();
-    }
-  });
-}
-function bindTooltips() {
-  $('div.skill-panel .skills-in-this-lesson').tooltip({
-    items: 'li.skill',
-    content: function() {
-      var description = $(this).data('skillDescription').trim();
-      if (description) {
-        return ('<b>' + $(this).text().trim() + '</b>: ' + description);
-      } else {
-        return null;
-      }
-    },
-    position: {
-      my: 'center top',
-      at: 'center bottom+10'
-    },
-    tooltipClass: 'skill-panel-tooltip'
-  });
-  $('div.skill-panel .skill-details .skill-card .description').tooltip({
-    items: '.skill-card',
-    content: function() {
-      var content = $(this).find('.content').text().trim();
-      if (content) {
-        return (
-            '<b>' + $(this).find('.name').text().trim() + '</b>: ' + content);
-      } else {
-        return null;
-      }
-    },
-    position: {
-      my: 'center top',
-      at: 'center bottom-40'
-    },
-    tooltipClass: 'skill-panel-tooltip'
-  });
-}
+    this._panelDiv.data(IS_INITIALIZED, true);
+  }
+};
+
 function init() {
-  var detailsPanel = $('div.skill-panel div.skill-details');
-  var openButton = $('div.skill-panel div.open-control button');
-
-  bindOpenButton();
-  bindSkillClick();
-  bindTooltips();
-  detailsPanel.hide();
-  openButton.addClass(ARROW_DOWN_CLASS);
-  dependencyMap = $('div.skill-panel').data('dependencyMap');
+  $('div.skill-panel').each(function() {
+    var skillPanelDiv = $(this);
+    if (! skillPanelDiv.data(IS_INITIALIZED)) {
+      new SkillPanel($(this)).init();
+    }
+  });
 }
 
-if ($('div.skill-panel').length > 0) {
-  init();
-}
+init();
