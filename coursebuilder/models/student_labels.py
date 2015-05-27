fbc309507df27ac4766f7b4d8a8fff831ae732d3
psimakov <psimakov@google.com>
2015-05-27 15:30:16 -0700

    Forgot to add this file to prior CL. 	Change on 2015/05/27 by psimakov <psimakov@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=94615153

--- coursebuilder/models/student_labels.py
+++ coursebuilder/models/student_labels.py
@@ -73,8 +73,8 @@ class StudentLabelsRestHandler(utils.ApplicationHandler):
             self._send_response(None, [], 403, 'No logged-in user')
             return None
         student = (
-            models.StudentProfileDAO.get_enrolled_student_by_email_for(
-                user.email(), self.app_context))
+            models.StudentProfileDAO.get_enrolled_student_by_user_for(
+                user, self.app_context))
         if not student or not student.is_enrolled:
             self._send_response(None, [], 403, 'User is not enrolled')
             return None
