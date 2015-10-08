1d9421a4166bc3f69072fcd5ad61b74d79932cd2
jorr <jorr@google.com>
2015-10-08 15:52:31 -0700

    Fix Site Admin link on Course Explorer. 	Change on 2015/10/08 by jorr <jorr@google.com> ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=105006638

--- coursebuilder/tests/functional/explorer_module.py
+++ coursebuilder/tests/functional/explorer_module.py
@@ -241,7 +241,7 @@ class CourseExplorerDisabledTest(actions.TestBase):
         response = self.get('/explorer')
         assert_contains('Explore Courses', response.body)
         assert_does_not_contain('My Courses', response.body)
-        assert_does_not_contain('Site admin', response.body)
+        assert_does_not_contain('Dashboard', response.body)
 
         # login and check logged in student perspective
         config.Registry.test_overrides[
@@ -252,7 +252,7 @@ class CourseExplorerDisabledTest(actions.TestBase):
         response = self.get('/explorer')
         assert_contains('Explore Courses', response.body)
         assert_contains('My Courses', response.body)
-        assert_does_not_contain('Site admin', response.body)
+        assert_does_not_contain('Dashboard', response.body)
 
     def test_admin_access(self):
         # enable course explorer
@@ -263,7 +263,8 @@ class CourseExplorerDisabledTest(actions.TestBase):
         # check the admin site link
         actions.login('admin@test.foo', is_admin=True)
         response = self.get('/explorer')
-        assert_contains('Site admin', response.body)
+        assert_contains(
+            '<a href="/modules/admin">Dashboard</a>', response.body)
 
 
 class GlobalProfileTest(BaseExplorerTest):
