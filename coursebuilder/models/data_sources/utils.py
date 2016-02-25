9e070f907f6598298a4e2aba0419427ec1dbda24
mgainer <mgainer@google.com>
2016-02-24 16:07:56 -0800

    Bump down warning levels to 'info'.  These are gunking up logs at NPTEL. 	Change on 2016/02/24 by mgainer <mgainer@google.com> ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=115504896

--- coursebuilder/models/data_sources/utils.py
+++ coursebuilder/models/data_sources/utils.py
@@ -31,16 +31,16 @@ def get_required_jobs(data_source_class, app_context, catch_and_log_):
     for required_generator in data_source_class.required_generators():
         job = required_generator(app_context).load()
         if not job:
-            catch_and_log_.critical('Job for %s has never run.' %
-                                    required_generator.__name__)
+            catch_and_log_.info('Job for %s has never run.' %
+                                required_generator.__name__)
             return None
         elif not job.has_finished:
-            catch_and_log_.critical('Job for %s is still running.' %
-                                    required_generator.__name__)
+            catch_and_log_.info('Job for %s is still running.' %
+                                required_generator.__name__)
             return None
         elif job.status_code == jobs.STATUS_CODE_FAILED:
-            catch_and_log_.critical('Job for %s failed its last run.' %
-                                    required_generator.__name__)
+            catch_and_log_.info('Job for %s failed its last run.' %
+                                required_generator.__name__)
             return None
         else:
             ret.append(job)
