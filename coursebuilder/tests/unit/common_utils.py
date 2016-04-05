221c74e228b4351b6511f07fc7b897e5d87cb52b
tlarsen <tlarsen@google.com>
2016-04-05 12:46:37 -0700

    Fix typo in module pydoc string: common.tags -> common.utils.
    	Change on 2016/04/05 by tlarsen <tlarsen@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=119079434

--- coursebuilder/tests/unit/common_utils.py
+++ coursebuilder/tests/unit/common_utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit tests for common.tags."""
+"""Unit tests for common.utils."""
 
 __author__ = 'Mike Gainer (mgainer@google.com)'
 
